1dd4871043b7745845a9b2628aebfc01a4bbe495
song <song@teach.works>
2015-12-03 11:37:31 +0800

    update vim

--- shell实例手册.sh
+++ shell实例手册.sh
@@ -118,32 +118,42 @@
 
     vim编辑器{
 
-        gconf-editor       # 配置编辑器
-        /etc/vimrc         # 配置文件路径
-        vim +24 file       # 打开文件定位到指定行
-        vim file1 file2    # 打开多个文件    
+        gconf-editor           # 配置编辑器
+        /etc/vimrc             # 配置文件路径
+        vim +24 file           # 打开文件定位到指定行
+        vim file1 file2        # 打开多个文件    
         vim -O2 file1 file2    # 垂直分屏
         vim -on file1 file2    # 水平分屏
-        sp filename        # 上下分割打开新文件
-        vsp filename       # 左右分割打开新文件
-        Ctrl+W [操作]      # 多个文件间操作  大写W  # 操作: 关闭当前窗口c  屏幕高度一样=  增加高度+  移动光标所在屏 右l 左h 上k 下j 中h  下一个w  
-        :n                 # 编辑下一个文件
-        :2n                # 编辑下二个文件
-        :N                 # 编辑前一个文件
-        :rew               # 回到首文件
-        :set nu            # 打开行号
-        :set nonu          # 取消行号
-        200G               # 跳转到200
-        :nohl              # 取消高亮
-        :set paste         # 取消缩进
-        :set autoindent    # 设置自动缩进
-        :set ff            # 查看文本格式
-        :set binary        # 改为unix格式
-        ctrl+ U            # 向前翻页
-        ctrl+ D            # 向后翻页
-        %s/字符1/字符2/g   # 全部替换    
-        X                  # 文档加密
-    
+        ctrl+ U                # 向前翻页
+        ctrl+ D                # 向后翻页
+        Ctrl+ww                # 在窗口间切换
+        Ctrl+w +or-or=         # 增减高度
+        :sp filename           # 上下分割打开新文件
+        :vs filename           # 左右分割打开新文件
+        :set nu                # 打开行号
+        :set nonu              # 取消行号
+        :nohl                  # 取消高亮
+        :set paste             # 取消缩进
+        :set autoindent        # 设置自动缩进
+        :set ff                # 查看文本格式
+        :set binary            # 改为unix格式
+        :%s/字符1/字符2/g      # 全部替换
+        :200                   # 跳转到200  1 文件头
+        G                      # 跳到行尾
+        dd                     # 删除当前行 并复制 可直接p粘贴
+        11111dd                # 删除11111行，可用来清空文件
+        r                      # 替换单个字符
+        R                      # 替换多个字符
+        u                      # 撤销上次操作
+        *                      # 全文匹配当前光标所在字符串
+        $                      # 行尾
+        0                      # 行首
+        X                      # 文档加密
+        v =                    # 自动格式化代码
+        Ctrl+v                 # 可视模式
+        Ctrl+v I ESC           # 多行操作
+        Ctrl+v s ESC           # 批量取消注释
+
     }
 
     归档解压缩{
